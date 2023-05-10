8dd4040ff2ff9adf416fe7cfeb0fac77a4062cd6
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-05-11 00:47:59 +0200

    feat: add about block

---
+++ src/pages/Welcome/components/about/About.tsx
@@ -0,0 +1,37 @@
+import { Button, Stack, Typography } from '@mui/material';
+import { generateText } from '~utils/generateParagraphs';
+
+import './About.scss';
+import { welcome } from '../../../../data/welcome-page';
+import { StackDirection } from '~interfaces/*';
+
+export const About: React.FC = () => {
+  const text = generateText(welcome.about.text);
+
+  return (
+    <Stack
+      spacing={{ xs: 1, sm: 2 }}
+      direction={StackDirection.row}
+      useFlexGap
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Stack direction={StackDirection.column} useFlexGap>
+        <div className="about-header">
+          <Typography variant="h4">
+            GraphQL IDE <br /> <small>for better development workflows!</small>
+          </Typography>
+        </div>
+        <div className="text">{text}</div>
+        <div>
+          <Button variant="contained" color="secondary">
+            Get Started
+          </Button>
+        </div>
+      </Stack>
+      <div>
+        <img className="image" src={welcome.about.link} alt="image" />
+      </div>
+    </Stack>
+  );
+};
