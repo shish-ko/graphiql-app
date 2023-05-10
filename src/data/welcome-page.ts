f0e54fef8a3e306656433812636f8e964d1128c5
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-05-11 00:47:38 +0200

    feat: add welcome page data and images

---
+++ src/data/welcome-page.ts
@@ -0,0 +1,31 @@
+export const welcome = {
+  about: {
+    text: {
+      paragraph1:
+        'GraphQL Playground is a graphical, interactive, in-browser GraphQL IDE, created by usage of React and based on GraphiQL.',
+      paragraph2:
+        'It is absolutely free. All you need to do is to create your own account. You are only few steps away from getting access to your own kanban boards.',
+      paragraph3: 'Click on the button below to begin.',
+    },
+    link: '/src/assets/welcome/about-img.webp',
+  },
+  info: {
+    text: {
+      paragraph1:
+        'GraphQL is a popular and widely used query language that bills itself as an alternative to the REST approach. One of the reasons why GraphQL is so well-regarded in the developer community is its thriving ecosystem.',
+      paragraph2:
+        'There are so many tools that make learning and using GraphQL easier. One of these tools is GraphQL Playground, a GraphQL integrated developer environment (IDE) that helps improve development workflows.',
+    },
+    link: '/src/assets/welcome/info-img.jpg',
+  },
+  rss: {
+    text: {
+      paragraph1: 'This application is a part of RS School Angular course.',
+      paragraph2:
+        'RS School is free-of-charge and community-based education program conducted by The Rolling Scopes developer community since 2013. The mentors and trainers of the school are front-end and javascript developers from different companies and countries.',
+      paragraph3:
+        'Everyone can study at RS School, regardless of age, professional employment, or place of residence. However, you should have sufficient base knowledge before the program begins. Want to learn more about RS School? You can visit their site <a href="https://rs.school">RS School</a>.',
+    },
+    link: '/src/assets/welcome/webdev.jpg',
+  },
+};
