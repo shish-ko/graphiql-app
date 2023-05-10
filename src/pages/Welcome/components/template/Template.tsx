01a399e55a743054bd88b3ab8e7c0ec78d26003a
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-05-11 00:48:16 +0200

    feat: add template for blocks

---
+++ src/pages/Welcome/components/template/Template.tsx
@@ -0,0 +1,28 @@
+import { ResponsiveStyleValue, Stack } from '@mui/system';
+import './Template.scss';
+import { generateText, Text } from '~utils/generateParagraphs';
+
+interface TemplateProps {
+  text: Text;
+  imgUrl: string;
+  direction: ResponsiveStyleValue<'row' | 'row-reverse'>;
+}
+
+export const Template: React.FC<TemplateProps> = (props: TemplateProps) => {
+  const text = generateText(props.text);
+
+  return (
+    <Stack
+      spacing={{ xs: 1, sm: 2 }}
+      direction={props.direction}
+      useFlexGap
+      alignItems="center"
+      justifyContent="center"
+    >
+      <div className="text">{text}</div>
+      <div>
+        <img className="image" src={props.imgUrl} alt="image" />
+      </div>
+    </Stack>
+  );
+};
