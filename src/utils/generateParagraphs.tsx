849474036b9f378bf7bbc2bac03c4a8b6e8d1b77
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-05-12 14:36:06 +0200

    fix: fix generateParagraph function

--- src/utils/generateParagraphs.tsx
+++ src/utils/generateParagraphs.tsx
@@ -1,13 +1,15 @@
+import { Box } from '@mui/material';
+
 export type Text = {
   [key: string]: string;
 };
 
 export const generateText = (textObject: Text) => {
   const text = Object.values(textObject).map((paragraph, indx) => (
-    <>
-      <p key={indx} dangerouslySetInnerHTML={{ __html: paragraph }}></p>
+    <Box key={indx}>
+      <p dangerouslySetInnerHTML={{ __html: paragraph }}></p>
       <br />
-    </>
+    </Box>
   ));
 
   return text;
