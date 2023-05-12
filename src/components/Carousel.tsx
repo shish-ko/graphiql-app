bd17c7a3772d94f4574756687e45cd2002f895cb
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-05-12 14:37:27 +0200

    fix: fix carousel width

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -9,7 +9,8 @@ export const ImageCarousel = ({ carousel }: CarouselProps) => {
   const carouselStyles = {
     maxWidth: '500px',
     maxHeight: '300px',
-    width: '45%',
+    minWidth: '45%',
+    width: '100%',
     height: '100%',
     margin: '0 auto',
   };
