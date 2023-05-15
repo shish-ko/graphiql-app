59b33a4950a927b7740be52e127aaf288b68f8b6
shish-ko <andrey.shishko@gmail.com>
2023-05-16 00:50:06 +0300

    add/ mainpage router loader

--- src/components/Documentation.tsx
+++ src/components/Documentation.tsx
@@ -1,29 +1,34 @@
 import { Box } from '@mui/material';
 import React, { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 import { IDoc, Schema, TypesEntity } from '~interfaces/doc_interfaces';
+import { useAlert } from '~utils/userHooks';
 import { DocItemList } from './DocItemList';
 
+export const loader = async () => {
+  const query =
+    'query IntrospectionQuery { __schema { queryType { name } mutationType { name } subscriptionType { name } types { ...FullType } directives { name description locations args { ...InputValue } } } } fragment FullType on __Type { kind name description fields(includeDeprecated: true) { name description args { ...InputValue } type { ...TypeRef } isDeprecated deprecationReason } inputFields { ...InputValue } interfaces { ...TypeRef } enumValues(includeDeprecated: true) { name description isDeprecated deprecationReason } possibleTypes { ...TypeRef } } fragment InputValue on __InputValue { name description type { ...TypeRef } defaultValue } fragment TypeRef on __Type { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name } } } } } } } }';
+  const res = await fetch('https://countries.trevorblades.com/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query }),
+  });
+  const { data }: IDoc = await res.json();
+  return data.__schema;
+};
+
 export const Documentation: React.FC = () => {
+  const schema = useLoaderData() as Schema;
   const [typeToDisplay, setTypeToDisplay] = useState<TypesEntity>();
-  const [schema, setSchema] = useState<Schema>();
+  const showMsg = useAlert();
 
   useEffect(() => {
-    const getSchema = async () => {
-      const query =
-        'query IntrospectionQuery { __schema { queryType { name } mutationType { name } subscriptionType { name } types { ...FullType } directives { name description locations args { ...InputValue } } } } fragment FullType on __Type { kind name description fields(includeDeprecated: true) { name description args { ...InputValue } type { ...TypeRef } isDeprecated deprecationReason } inputFields { ...InputValue } interfaces { ...TypeRef } enumValues(includeDeprecated: true) { name description isDeprecated deprecationReason } possibleTypes { ...TypeRef } } fragment InputValue on __InputValue { name description type { ...TypeRef } defaultValue } fragment TypeRef on __Type { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name } } } } } } } }';
-      const res = await fetch('https://countries.trevorblades.com/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ query }),
-      });
-      const { data }: IDoc = await res.json();
-      setSchema(data.__schema);
-      const queryTypes = data.__schema.types!.find((item) => item.name.match(/query/i));
-      queryTypes ? setTypeToDisplay(queryTypes) : console.log('No query types');
-    };
-    getSchema();
+    const queryTypes = schema.types!.find((item) => item.name.match(/query/i));
+    queryTypes
+      ? setTypeToDisplay(queryTypes)
+      : showMsg({ type: 'error', content: 'Something went wrong...' });
   }, []);
 
   return (
