96748b51e3edbe161e2d671d2966cfd23b2816c6
shish-ko <shishko.dev@gamil.com>
2023-05-25 23:27:29 +0300

    fix: header nowrap to fit all elements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -56,7 +56,7 @@ export const Header: React.FC = () => {
               justifyContent: 'space-between',
               alignItems: 'end',
               gap: '10px',
-              flexWrap: 'wrap',
+              flexWrap: 'nowrap',
             }}
           >
             <Logo />
